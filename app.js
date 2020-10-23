4cc1595f3530ad0aacc17a08cc8b128444e7b8c0
ioaneN <66302128+ioaneN@users.noreply.github.com>
2020-10-23 14:45:22 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+const stars = () => {
+    let count = 500;
+    let scene = document.querySelector(".scene");
+    let i = 0;
+    while (i < count) {
+      let star = document.createElement("i");
+      let x = Math.floor(Math.random() * window.innerWidth);
+      let y = Math.floor(Math.random() * window.innerHeight);
+      let duration = Math.random() * 10;
+      let size = Math.random() * 2;
+  
+      star.style.left = x + "px";
+      star.style.top = y + "px";
+      star.style.width = 1 + size + "px";
+      star.style.height = 1 + size + "px";
+  
+      star.style.animationDuration = 5 + duration + "s";
+      scene.appendChild(star);
+      i++;
+    }
+  };
+  stars();
+
+
+  //clock logic
+
+   
+setInterval(setClock, 1000)
+
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
+
+function setClock() {
+  const currentDate = new Date()
+  const secondsRatio = currentDate.getSeconds() / 60
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+  setRotation(secondHand, secondsRatio)
+  setRotation(minuteHand, minutesRatio)
+  setRotation(hourHand, hoursRatio)
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setClock()
